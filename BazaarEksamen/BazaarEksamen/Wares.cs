615ebc91afe6c66b5bb768bc164035f45e41dbae
hermananker <hermananker1@gmail.com>
2017-11-18 15:19:31 +0100

    fjernet unødvendig feilkode

--- BazaarEksamen/BazaarEksamen/Wares.cs
+++ BazaarEksamen/BazaarEksamen/Wares.cs
@@ -10,13 +10,7 @@ namespace BazaarEksamen
     class Wares
     {
         public static Wares WareObject = new Wares();
-        //private int testWare;
 
-        // public int TestWare { get; set; }
-        // {
-        // get { return testWare; }
-        //set { testWare = value; }
-        //}
 
         private void Ware(int wareToPrint)
         {
@@ -30,8 +24,6 @@ namespace BazaarEksamen
         public static void MakeWare()
         {
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            //lock (WareObject)
-            // {
             for (int i = 1; i < numbers.Length; i++)
             {
                 for (int y = 1; y < numbers.Length; y++)
@@ -45,26 +37,9 @@ namespace BazaarEksamen
                         }
                     }
                 }
-                //Console.ReadLine();
             }
-            //}
 
         }
 
-        /*public static void Sold()
-        {
-            List<int> items = new List<int> { 1, 2, 3, 4, 5 };
-            for (int i = 1; i < 5; i++)
-            {
-                Wares sellWare = new Wares();
-                sellWare.TestWare = i;
-                Console.WriteLine("Bazaar puts their item #" + i + " for sale");
-            }
-        }
-
-       public static void SellWares()
-        {
-            Console.WriteLine("{0} buys item: #" + i, Thread.CurrentThread.Name);
-        }*/
     }
 }
